f0c11fe825fe9a2d9ef81ac04d9cb0c795a0370f
sa02045 <sa02045@naver.com>
2023-11-19 09:33:02 +0900

    Del 코드정리

--- src/api/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-import type { Menu } from "../types/Menu";
-
-export async function fetchCarts() {
-  const { data } = await axios.get<Menu>("/carts");
-  return data;
-}
--- src/mocks/mockData.ts
+++
@@ -1,5 +0,0 @@
-export const menus = [
-  { name: "짜장면", price: 5000 },
-  { name: "짬뽕", price: 6000 },
-  { name: "탕수육", price: 12000 },
-];
