ae62c7b773a58f5f48fe01f44163bda61b86af26
sa02045 <sa02045@naver.com>
2023-11-19 09:51:56 +0900

    del 코드 정리

--- src/week1/calculate.ts
+++
@@ -1,3 +0,0 @@
-export const minus = (a: number, b: number) => a - b;
-export const multiply = (a: number, b: number) => a * b;
-export const divide = (a: number, b: number) => a / b;
--- src/week1/delayFunction.ts
+++
@@ -1,3 +0,0 @@
-export const delayFunction = (callback: () => void, delay: number) => {
-  setTimeout(callback, delay);
-};
